4135ac1447ce6f0355af031e9bbd27a39a0ea6fb
Lacuran <kornel.wrzesien@gmail.com>
2021-10-19 12:46:52 +0200

    19.10.2021 added hotel page and hotel tests for selenium task

--- src/main/java/pages/HotelsPage.java
+++ src/main/java/pages/HotelsPage.java
@@ -4,17 +4,37 @@ import BaseElements.BaseAbstractPage;
 import Core.TAEDriver;
 import Entities.HotelsSearch;
 import Entities.Travellers;
+import forms.HotelForm;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import uicontrols.CustomSelect;
 import uicontrols.TravellersControl;
 
 
 public class HotelsPage extends BaseAbstractPage {
-    public static String TITLE = "Search Hotels - PHPTRAVELS";
+    public static String TITLE = "PHPTRAVELS - PHPTRAVELS";
     CustomSelect cityDropdown;
+
+    @FindBy(xpath = "//*[@id='rooms']")
+    public WebElement rooms;
+    @FindBy(xpath = "//*[@id='adults']")
+    public WebElement adults;
+    @FindBy(xpath = "//*[@id='childs']")
+    public WebElement children;
+    @FindBy(xpath = "//*[@id='nationality']")
+    public WebElement nationality;
+    @FindBy(xpath = "//*[@class='dropdown-menu dropdown-menu-wrap']/..")
+    public WebElement boxDropdownForHotels;
+    @FindBy(xpath = "//*[@id='submit']")
+    public WebElement searchHotelButton;
+    @FindBy(xpath = "//*[@*='no results']")
+    public WebElement noMatchFound;
 
     public HotelsPage(TAEDriver driver) {
         super(driver, TITLE);
         cityDropdown = new CustomSelect("HotelCity", driver, "//span[@role='combobox']");
+        PageFactory.initElements(driver.getDriver(), this);
     }
 
     public HotelsPage selectCity(String cityName) {
@@ -23,17 +43,23 @@ public class HotelsPage extends BaseAbstractPage {
     }
 
     public HotelsPage setTravellers(Travellers travellers) {
-
+        clearForm();
+        rooms.sendKeys(String.valueOf(travellers.getRooms()));
+        adults.sendKeys(String.valueOf(travellers.getAdults()));
+        children.sendKeys(String.valueOf(travellers.getChild()));
         return this;
     }
 
     public HotelsPage submit() {
-
+        searchHotelButton.click();
         return this;
     }
 
     public HotelsPage setTravellersWithNationality(Travellers travellers){
-
+        rooms.sendKeys(String.valueOf(travellers.getRooms()));
+        adults.sendKeys(String.valueOf(travellers.getAdults()));
+        children.sendKeys(String.valueOf(travellers.getChild()));
+        nationality.sendKeys(travellers.getNationality());
         return this;
     }
 
@@ -47,5 +73,21 @@ public class HotelsPage extends BaseAbstractPage {
         fillPage(hotelsSearch);
         submit();
         return this;
+    }
+
+    public HotelsPage clearForm(){
+        rooms.clear();
+        adults.clear();
+        children.clear();
+        return this;
+    }
+
+    public HotelsPage clickDropBoxHotelSetting(){
+        boxDropdownForHotels.click();
+        return this;
+    }
+
+    public boolean noMatchFoundIsDisplayed(){
+        return noMatchFound.isDisplayed();
     }
 }
--- src/test/java/gloTests/HotelsTests.java
+++ src/test/java/gloTests/HotelsTests.java
@@ -1,19 +1,13 @@
 package gloTests;
 
 import BaseTest.BaseAbstractTest;
-import Entities.HotelsSearch;
 import Entities.Travellers;
 import TAExceptions.TAUnknownBrowserException;
-import forms.HotelForm;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import pages.HomePage;
 import pages.HotelsPage;
 import uicontrols.CustomSelect;
-import uicontrols.TravellersControl;
-
-import java.sql.Driver;
-import java.util.concurrent.TimeUnit;
 
 public class HotelsTests extends BaseAbstractTest {
 
@@ -21,40 +15,41 @@ public class HotelsTests extends BaseAbstractTest {
         super("chrome");
     }
 
-    @Test
-    public void testHotelsPage() {
+    @BeforeTest
+    public void setUp(){
         getDriver().navigateTo("https://www.phptravels.net/");
-
         String city = "Krakow";
+        CustomSelect customSelect = new CustomSelect("HotelCity", getDriver(), "(//*[@role='combobox'])[1]");
+        customSelect.selectItem(city);
+    }
+
+    @Test
+    public void shouldGetMassageNoMatchFound() {
         Travellers travellers = new Travellers(5,10,0);
 
-        HotelsSearch hotelsSearch = new HotelsSearch();
+        HotelsPage hotelsPage = new HotelsPage(getDriver());
+        hotelsPage.clickDropBoxHotelSetting();
+        hotelsPage.clearForm();
+        hotelsPage.setTravellers(travellers);
+        hotelsPage.submit();
 
-        new HomePage(getDriver()).switchToHotelsPage().fillPageAndSearch(hotelsSearch);
-        HotelForm hotelForm = new HotelForm(getDriver());
-        TravellersControl travellersControl = new TravellersControl("Hotel", getDriver(), "//*[@class='dropdown-menu dropdown-menu-wrap']/..");
-        hotelForm.clickDropBoxHotelSetting();
-        travellersControl.setTravellers(travellers);
-
+        Assert.assertTrue(hotelsPage.noMatchFoundIsDisplayed(),"Message with no result is not displayed");
 
     }
 
     @Test
-    public void testHotelsPage2() {
-        getDriver().navigateTo("https://www.phptravels.net/");
-        CustomSelect customSelect = new CustomSelect("HotelCity", getDriver(), "(//*[@role='combobox'])[1]");
-        customSelect.selectItem("Krakow");
+    public void shouldSetUpTravellersWithNationality() {
+        String nationality = "Poland";
 
-        Travellers travellers = new Travellers(5,10,0);
+        Travellers travellers = new Travellers(5,10,0, nationality);
 
-        HotelForm hotelForm = new HotelForm(getDriver());
+        HotelsPage hotelsPage = new HotelsPage(getDriver());
+        hotelsPage.clickDropBoxHotelSetting();
+        hotelsPage.clearForm();
+        hotelsPage.setTravellersWithNationality(travellers);
+        hotelsPage.submit();
 
-        TravellersControl travellersControl = new TravellersControl("Hotel", getDriver(), "//*[@class='dropdown-menu dropdown-menu-wrap']/..");
-        hotelForm.clickDropBoxHotelSetting();
-        travellersControl.setTravellers(travellers);
-        getDriver().getDriver().manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        hotelForm.searchHotelButton.click();
-
+        Assert.assertTrue(hotelsPage.noMatchFoundIsDisplayed(),"Message with no result is not displayed");
 
     }
 }
